f292efd564a168d49aab4ab08b7cb42902dade90
Song, Gyu <songkyoo@hotmail.com>
2025-03-02 01:26:38 +0900

    Uncurry 확장 메서드 추가

---
+++ Functional.Tests/UncurryTest.cs
@@ -0,0 +1,19 @@
+namespace Macaron.Tests.Functional;
+
+using Macaron.Functional;
+
+[TestFixture]
+public class UncurryTest
+{
+    [Test]
+    public void Uncurry_WithCurriedFunction_ReturnsUncurriedFunction()
+    {
+        var fn = (int x, int y, int z) => x + y + z;
+        var curriedFn = fn.Curry(); // x => y => z => fn(x, y, z);
+
+        var oneAppliedFn = curriedFn(1); // y => z => fn(1, y, z);
+        var uncurriedFn = oneAppliedFn.Uncurry(); // (int x, int y) => fn(x, y, z);
+
+        Assert.That(uncurriedFn(2, 3), Is.EqualTo(6));
+    }
+}
