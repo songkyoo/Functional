8f0d01b6bbb626c1c6f780ac498a5c5481c8f55c
Song, Gyu <songkyoo@hotmail.com>
2025-03-05 08:03:21 +0900

    Context를 받는 오버로딩을 제거

--- Functional/Extensions.cs
+++ Functional/Extensions.cs
@@ -7,21 +7,9 @@ public static partial class Extensions
         return fn(self);
     }
 
-    public static TResult Let<T, TContext, TResult>(this T self, in TContext context, Func<TContext, T, TResult> fn)
-        where T : notnull
-    {
-        return fn(context, self);
-    }
-
     public static T Also<T>(this T self, Action<T> action) where T : notnull
     {
         action(self);
-        return self;
-    }
-
-    public static T Also<T, TContext>(this T self, in TContext context, Action<TContext, T> action) where T : notnull
-    {
-        action(context, self);
         return self;
     }
 
@@ -30,21 +18,9 @@ public static partial class Extensions
         return predicate(self) ? self : null;
     }
 
-    public static T? TakeIf<T, TContext>(this T self, TContext context, Func<TContext, T, bool> predicate)
-        where T : class
-    {
-        return predicate(context, self) ? self : null;
-    }
-
     public static T? TakeIfStruct<T>(this T self, Func<T, bool> predicate) where T : struct
     {
         return predicate(self) ? self : null;
-    }
-
-    public static T? TakeIfStruct<T, TContext>(this T self, in TContext context, Func<TContext, T, bool> predicate)
-        where T : struct
-    {
-        return predicate(context, self) ? self : null;
     }
 
     public static T? TakeUnless<T>(this T self, Func<T, bool> predicate) where T : class
@@ -52,21 +28,9 @@ public static partial class Extensions
         return !predicate(self) ? self : null;
     }
 
-    public static T? TakeUnless<T, TContext>(this T self, in TContext context, Func<TContext, T, bool> predicate)
-        where T : class
-    {
-        return !predicate(context, self) ? self : null;
-    }
-
     public static T? TakeUnlessStruct<T>(this T self, Func<T, bool> predicate) where T : struct
     {
         return !predicate(self) ? self : null;
-    }
-
-    public static T? TakeUnlessStruct<T, TContext>(this T self, in TContext context, Func<TContext, T, bool> predicate)
-        where T : struct
-    {
-        return !predicate(context, self) ? self : null;
     }
 
     public static TResult Use<T, TResult>(this T disposable, Func<T, TResult> fn) where T : IDisposable
@@ -74,18 +38,6 @@ public static partial class Extensions
         using (disposable)
         {
             return fn(disposable);
-        }
-    }
-
-    public static TResult Use<T, TContext, TResult>(
-        this T disposable,
-        in TContext context,
-        Func<TContext, T, TResult> fn
-    ) where T : IDisposable
-    {
-        using (disposable)
-        {
-            return fn(context, disposable);
         }
     }
 }
