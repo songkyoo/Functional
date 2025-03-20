948595fbec8fd7c2c42ded3b07402be8c958c91b
Song, Gyu <songkyoo@hotmail.com>
2025-03-20 19:46:52 +0900

    Curry, Uncurry, Partial 정적 메서드 추가

--- Functional/Utility.Pair.cs
+++ Functional/Utility.Pair.cs
@@ -2,20 +2,38 @@ namespace Macaron.Functional;
 
 partial class Utility
 {
-    public static KeyValuePair<TKey, TValue> Pair<TKey, TValue>(TKey key, TValue value)
+    public static KeyValuePair<TKey, TValue> Pair<TKey, TValue>(
+        TKey key, TValue value
+    )
     {
         return new KeyValuePair<TKey, TValue>(key, value);
     }
 
-    public static Action<KeyValuePair<TKey, TValue>> ToPaired<TKey, TValue>(Action<TKey, TValue> action)
+    public static Action<KeyValuePair<TKey, TValue>> ToPaired<TKey, TValue>(
+        Action<TKey, TValue> action
+    )
     {
         return pair => action(pair.Key, pair.Value);
     }
 
-    public static Func<KeyValuePair<TKey, TValue>, TResult> FromPaired<TKey, TValue, TResult>(
+    public static Func<KeyValuePair<TKey, TValue>, TResult> ToPaired<TKey, TValue, TResult>(
         Func<TKey, TValue, TResult> fn
     )
     {
         return pair => fn(pair.Key, pair.Value);
+    }
+
+    public static Action<TKey, TValue> FromPaired<TKey, TValue>(
+        Action<KeyValuePair<TKey, TValue>> action
+    )
+    {
+        return (key, value) => action(Pair(key, value));
+    }
+
+    public static Func<TKey, TValue, TResult> FromPaired<TKey, TValue, TResult>(
+        Func<KeyValuePair<TKey, TValue>, TResult> fn
+    )
+    {
+        return (key, value) => fn(Pair(key, value));
     }
 }
